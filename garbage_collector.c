17f300cb105caad2f3c1ad0ad32cc4c1703778e0
Mohamed Boussetta <mbousset@e1r11p5.1337.ma>
2025-04-10 07:59:08 +0100

    hello

---
+++ garbage_collector.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   garbage_collector.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salahian <salahian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 14:53:05 by salahian          #+#    #+#             */
+/*   Updated: 2025/04/09 16:31:55 by salahian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_gar   **garbage_list(void)
+{
+    static t_gar *tail;
+    return (&tail);
+}
+
+void    add_to_the_list(t_gar **tail, t_gar *new)
+{
+    if (!tail || !*tail)
+        *tail = new;
+    new->next = *tail;
+}
+
+void    *ft_malloc(int size, int bytes)
+{
+    void    *p;
+    t_gar   *new;
+
+    new = malloc(sizeof(t_gar));
+    if (!new)
+        return (NULL);
+    new->add = malloc(size * bytes);
+    if (!new->add)
+        return (free(new), NULL);
+    p = new->add;
+    new->next = NULL;
+    add_to_the_list(garbage_list(), new);
+    return(p);
+}
