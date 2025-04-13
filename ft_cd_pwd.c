14d9c910485ef236a29b096dfa4fbe2480c2fe5d
Soulaimane Alahiane <salahian@e1r12p6.1337.ma>
2025-04-13 18:01:55 +0100

    i add export

--- ft_cd_pwd.c
+++ ft_cd_pwd.c
@@ -6,13 +6,13 @@
 /*   By: salahian <salahian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 14:40:08 by salahian          #+#    #+#             */
-/*   Updated: 2025/04/12 16:44:14 by salahian         ###   ########.fr       */
+/*   Updated: 2025/04/13 15:10:43 by salahian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void update_pwd(t_env **env_list)
+int ft_pwd(t_env **env_list)
 {
     char *cwd;
     t_env *cur;
@@ -21,7 +21,7 @@ void update_pwd(t_env **env_list)
     if (!cwd)
     {
         perror("getcwd");
-        return;
+        return (0);
     }
     cur = *env_list;
     while (cur)
@@ -35,14 +35,52 @@ void update_pwd(t_env **env_list)
         cur = cur->next;
     }
     free(cwd);
+    return (1);
+}
+
+int update_pwd(t_env **env_list, char *cwd)
+{
+	t_env *cur;
+
+    cur = *env_list;
+	while (cur)
+	{
+		if (strcmp(cur->key, "PWD") == 0)
+		{
+			free(cur->value);
+			cur->value = strdup(cwd);
+			if (!cur->value)
+			{
+				perror("strdup");
+				return (0);
+			}
+			return (1);
+		}
+		cur = cur->next;
+	}
+	return (0);
 }
 
 int ft_cd(char *path, t_env **env_list)
 {
-    if (!path)
-        return (printf("cd: path required\n"), 1);
-    if (chdir(path) != 0)
-        return (perror("cd"), 1);
-    update_pwd(env_list);
-    return 0;
+	char *cwd;
+
+	if (!path)
+		return (write(1, "cd: path required\n", 19), 1);
+	if (chdir(path) != 0)
+		return (perror("cd"), 1);
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		perror("cd: getcwd failed (directory structure might be broken)");
+		return (1);
+	}
+	if (!update_pwd(env_list, cwd))
+	{
+		write(2, "cd: failed to update PWD\n", 26);
+		free(cwd);
+		return (1);
+	}
+	free(cwd);
+	return 0;
 }
--- ft_export.c
+++ ft_export.c
@@ -6,9 +6,177 @@
 /*   By: salahian <salahian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 17:10:17 by salahian          #+#    #+#             */
-/*   Updated: 2025/04/12 17:10:30 by salahian         ###   ########.fr       */
+/*   Updated: 2025/04/13 18:01:30 by salahian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// int not_valid(char c)
+// {
+//     return ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') ||
+//     (c >= 'a' && c <= 'z') || c == '_');
+// }
+// int check_is_okey(char *s)
+// {
+//     int     i;
+
+//     i = 0;
+//     while (s[i])
+//     {
+//         if (s[i] == '=')
+//             break ;
+//         if (!not_valid(s[i]))
+//             return (0);
+//         i++;
+//     }
+//     return (1);
+// }
+
+// t_env    *create_new(char *s, int sep)
+// {
+//     t_env   *node;
+    
+//     node = ft_malloc(sizeof(t_env), 1);
+//     node->key = ft_substr(s, 0, sep);
+//     node->value = ft_substr(s, sep + 1, ft_strlen(s) - (sep + 1));
+//     node->next = NULL;
+//     return (node);
+// }
+
+// void    add_the_new(t_env **env, t_env *new)
+// {
+//     t_env   *tmp;
+
+//     tmp = env;
+//     while (tmp)
+//         tmp = tmp->next;
+//     tmp->next = new;
+// }
+
+// int	ft_export(char **args, t_env **env)
+// {
+//     int     i;
+//     int     j;
+//     char    *s;
+//     t_env   *tmp;
+
+//     i = 1;
+//     if (!args[1])
+//         return (print_env(env), 0);
+//     if ((args[1][0] >= '0' && args[1][0] <= '9') || !check_is_okey(args[1]))
+//     {
+//         write(2, "minishell: export: not a valid identifier\n", 43);
+//         return (1);
+//     }
+//     tmp = *env;
+//     while (tmp)
+//     {
+//         j = 0;
+//         while (args[i][j])
+//         {
+//             if (args[i][j] == '=')
+//             {
+//                 if (ft_strncmp(args[i], tmp->key, ft_strlen(tmp->key) == 0))
+//                 {
+//                     s = tmp->value;
+//                     tmp->value = ft_strdup(&args[i][j + 1]);
+//                     free(s);
+//                 }
+//                 else
+//                     add_the_new(env, create_new(args[i], j));
+//                 return (0);
+//             }
+//             j++;
+//         }
+//         tmp = tmp->next;
+//     }
+//     return (0);
+// }
+
+int not_valid(char c)
+{
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
+            (c >= '0' && c <= '9') || c == '_');
+}
+
+int check_is_okey(char *s)
+{
+    int i = 0;
+
+    if (!not_valid(s[0]) || (s[0] >= '0' && s[0] <= '9')) // can't start with digit
+        return (0);
+    while (s[i] && s[i] != '=')
+    {
+        if (!not_valid(s[i]))
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+t_env *create_new(char *s, int sep)
+{
+    t_env *node;
+
+    node = ft_malloc(sizeof(t_env), 1);
+    node->key = ft_substr(s, 0, sep);
+    node->value = (s[sep] == '=') ? ft_substr(s, sep + 1, ft_strlen(s) - sep - 1) : NULL;
+    node->next = NULL;
+    return (node);
+}
+
+void add_the_new(t_env **env, t_env *new)
+{
+    t_env *tmp;
+
+    if (!*env)
+        *env = new;
+    else
+    {
+        tmp = *env;
+        while (tmp->next)
+            tmp = tmp->next;
+        tmp->next = new;
+    }
+}
+
+int ft_export(char **args, t_env **env)
+{
+    int i = 1;
+    int sep;
+    t_env *tmp;
+
+    while (args[i])
+    {
+        if (!check_is_okey(args[i]))
+        {
+            write(2, "minishell: export: not a valid identifier\n", 43);
+            i++;
+            continue;
+        }
+
+        sep = 0;
+        while (args[i][sep] && args[i][sep] != '=')
+            sep++;
+
+        tmp = *env;
+        while (tmp)
+        {
+            if (ft_strncmp(args[i], tmp->key, sep) == 0 && tmp->key[sep] == '\0')
+            {
+                if (args[i][sep] == '=')
+                {
+                    free(tmp->value);
+                    tmp->value = ft_strdup(&args[i][sep + 1]);
+                }
+                break;
+            }
+            tmp = tmp->next;
+        }
+        if (!tmp)
+            add_the_new(env, create_new(args[i], sep));
+        i++;
+    }
+    return (0);
+}
