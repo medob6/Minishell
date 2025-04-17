cf76f7050d789c90eef6dd325828e4259d507f61
Mohamed Boussetta <mbousset@e1r11p3.1337.ma>
2025-04-17 18:14:35 +0100

    fixed bugs in parser

---
+++ f
@@ -0,0 +1,7 @@
+total 168
+-rw-r--r-- 1 mbousset candidates  2868 Apr 16 17:08 create_the_main_list.c
+-rw-r--r-- 1 mbousset candidates  1464 Apr 17 10:50 garbage_collector.c
+-rw-r--r-- 1 mbousset candidates  6327 Apr 17 10:50 lexem.c
+-rw-r--r-- 1 mbousset candidates   348 Apr 17 10:50 Makefile
+-rw-r--r-- 1 mbousset candidates  1276 Apr 17 16:34 minishell.h
+drwxr-xr-x 2 mbousset candidates  4096 Apr 17 10:50 utils_lib
--- parsing.c
+++ parsing.c
@@ -14,10 +14,13 @@ t_ast_node	*creat_ast_node(int node_type)
 
 t_ast_node	*simple_command(t_token **token)
 {
+	static int	i;
 	t_ast_node	*simple_cmd;
 
+	i++;
+	printf("token : %s , i = %d\n", get_value((*token)->type), i);
 	simple_cmd = creat_ast_node(AST_SIMPLE_CMD);
-	while ((*token)->type != TOKEN_EOF)
+	while (true)
 	{
 		if (is_redirction((*token)->type))
 		{
@@ -27,26 +30,29 @@ t_ast_node	*simple_command(t_token **token)
 		}
 		else if ((*token)->type == TOKEN_WORD)
 			add_child(simple_cmd, (*token)->value);
+		else if ((*token)->type == TOKEN_TO_EXPAND)
+			add_child(simple_cmd, (*token)->value);
 		else
 			break ;
 		advance_token(token);
 	}
-	if ((!simple_cmd->children && !simple_cmd->children))
+	// printf("simpl cmd 1 : %p\n", simple_cmd->children);
+	if ((!simple_cmd->children && !simple_cmd->redirect_list))
+	{
+		// printf("simpl cmd 2: %p\n", simple_cmd->children);
+		// printf("returning NULL \n");
+		// printf("TOKEN IS : %s", (*token)->value);
 		return (NULL);
-	if ((*token)->type == TOKEN_PARENTESIS_CLOSE)
-		advance_token(token);
+	}
 	return (simple_cmd);
 }
-
 bool	paranteses_symetric(t_token **token)
 {
 	t_token	*temp_head;
 
 	int count_open, count_close;
-	count_open = 1;
+	count_open = 0;
 	count_close = 0;
-	if ((*token)->type == TOKEN_PARENTESIS_OPEN)
-		advance_token(token);
 	temp_head = *token;
 	while (temp_head->type != TOKEN_EOF)
 	{
@@ -54,46 +60,50 @@ bool	paranteses_symetric(t_token **token)
 			count_open++;
 		if (temp_head->type == TOKEN_PARENTESIS_CLOSE)
 			count_close++;
-		if (count_close == count_open)
-		{
-			printf("symtric\n");
-			return (true);
-		}
-		printf("open : %d, close : %d \n", count_open, count_close);
+		if (count_close > count_open)
+			return (false);
 		temp_head = (temp_head)->next;
 	}
+	if (count_close == count_open)
+		return (true);
 	return (false);
 }
 t_ast_node	*subshell(t_token **token)
 {
 	t_ast_node	*compouned;
 
-	// here i check if this current open parantes has a close symtric to it if not return null syntax error else we remove them and call compuned on the current
-	if (!paranteses_symetric(token))
-	{
-		printf("unsymtric\n");
+	// subshell         : '(' command_list ')' redirect_list*
+	if ((*token)->type != TOKEN_PARENTESIS_OPEN)
 		return (NULL);
-	}
-	compouned = compound_cmd(token);
+	advance_token(token);
 	// if ((*token)->type == TOKEN_PARENTESIS_CLOSE)
-	// {
-	// 	printf("closed\n");
-	// 	advance_token(token);
-	// 	if ((*token)->type == TOKEN_EOF)
-	// 		printf("eof\n");
-	// }
-	// else
-	// 	return (NULL);
+	// 	printf("WAAAAAAAAAAAA######\n");
+	compouned = compound_cmd(token, AST_SUBSHELL);
+	if ((*token)->type != TOKEN_PARENTESIS_CLOSE)
+		return (NULL);
+	advance_token(token);
+	while (true)
+	{
+		if (is_redirction((*token)->type))
+		{
+			if (!(*token)->value)
+				return (NULL);
+			add_redirect(compouned, *token);
+		}
+		else
+			break ;
+		advance_token(token);
+	}
 	return (compouned);
 }
 
 t_ast_node	*command(t_token **token)
 {
-	if ((*token)->type == TOKEN_PARENTESIS_OPEN)
-	{
-		// advance_token(token);
-		return (subshell(token));
-	}
+	t_ast_node	*command;
+
+	command = subshell(token);
+	if (command)
+		return (command);
 	return (simple_command(token));
 }
 
@@ -116,19 +126,26 @@ t_ast_node	*pipeline(t_token **token)
 	return (pipe_node);
 }
 
-t_ast_node	*compound_cmd(t_token **token)
+t_ast_node	*compound_cmd(t_token **token, t_ast_type type)
 {
 	t_ast_node	*compound;
 	t_ast_node	*current;
 	t_ast_type	logic_type;
+	static int	i;
 
-	compound = creat_ast_node(AST_COMPOUNED_CMD);
+	i++;
+	compound = creat_ast_node(type);
 	while (*token && (*token)->type != TOKEN_EOF)
 	{
+		// if ((*token)->type == TOKEN_PARENTESIS_CLOSE)
+		// 	break ;
+		// printf("depth= %d pipeline b TOKEN IS  : %s\n", i,
+		// 	get_value((*token)->type));
 		current = pipeline(token);
-		printf("token_type == %s\n", get_value((*token)->type));
+		// printf("depth= %d  pipeline a TOKEN IS return (val : %p : %s\n", i,
+		// 	current, get_value((*token)->type));
 		if (!current)
-			return (NULL);
+			return (i--, (NULL));
 		add_child(compound, current);
 		if ((*token)->type == TOKEN_AND || (*token)->type == TOKEN_OR)
 		{
@@ -140,11 +157,13 @@ t_ast_node	*compound_cmd(t_token **token)
 			add_child(compound, current);
 			advance_token(token);
 			if ((*token)->type == TOKEN_EOF)
-				return (NULL);
+				return (i--, (NULL));
 		}
-		if ((*token)->type != TOKEN_EOF)
-			printf("token_type == %s\n", get_value((*token)->type));
+		else if ((*token)->type == TOKEN_PARENTESIS_CLOSE)
+			break ;
+		// TODO: test parser more fix this test  : (ls) > s or (ls) >> d
 	}
+	i--;
 	return (compound);
 }
 
@@ -152,7 +171,9 @@ t_ast_node	*parse_tokens(t_token *tokens)
 {
 	t_ast_node	*root;
 
-	root = compound_cmd(&tokens);
+	if (!paranteses_symetric(&tokens))
+		return (NULL);
+	root = compound_cmd(&tokens,AST_COMPOUNED_CMD);
 	if (!root)
 		return (NULL);
 	return (root);
