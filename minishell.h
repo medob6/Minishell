391883f051b44a95e8c5372a6e892d0a61232738
Soulaimane Alahiane <salahian@e1r12p6.1337.ma>
2025-04-17 18:21:15 +0100

    a

--- minishell.h
+++ minishell.h
@@ -54,7 +54,6 @@ typedef struct s_env
 typedef enum e_token_type
 {
 	TOKEN_WORD,
-	TOKEN_WILDCARDS, // *
 	TOKEN_PARENTESIS_OPEN, //(
 	TOKEN_PARENTESIS_CLOSE,
 	TOKEN_AND,
@@ -64,7 +63,6 @@ typedef enum e_token_type
 	TOKEN_REDIRECT_OUT, // >
 	TOKEN_APPEND,       // >>
 	TOKEN_HEREDOC,      // <<
-	TOKEN_TO_EXPAND,
 	TOKEN_EOF
 }			t_token_type;
 
