569385bcb17ef5311aad410ef0f30e378c89a6fc
Mohamed Boussetta <mbousset@e1r12p6.1337.ma>
2025-04-18 18:22:04 +0100

    test upgraded with colors

---
+++ execution/execute_cmd_line.c
@@ -0,0 +1,10 @@
+#include "execution.h"
+
+// here i am gona code a set of functions that execute commands
+
+void	execute_cmd_line(t_ast_node *root)
+{
+	// expaned before excute
+
+	return ;
+}
---
+++ execution/execution.h
@@ -0,0 +1,11 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include "../parser.h"
+
+void execute_cmd_line(t_ast_node *root);
+// here must be expansion unc or header file
+
+#endif
+
+
