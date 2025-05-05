60c05fdd7da143bc62adbd1f344b2236efb15963
Mohamed Boussetta <mbousset@c3r7p9.1337.ma>
2025-05-05 17:48:29 +0100

    splited execution functions into multipel files

---
+++ execution/redirect_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbousset <mbousset@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 17:00:15 by mbousset          #+#    #+#             */
+/*   Updated: 2025/05/05 17:02:52 by mbousset         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execution.h"
+
+int	open_file(t_token *file_token)
+{
+	if (!file_token)
+		return (-1);
+	if (file_token->type == TOKEN_REDIRECT_IN)
+		return (open(file_token->value.str_value, O_RDONLY));
+	else if (file_token->type == TOKEN_REDIRECT_OUT)
+		return (open(file_token->value.str_value, O_WRONLY | O_CREAT | O_TRUNC,
+				0644));
+	else if (file_token->type == TOKEN_APPEND)
+		return (open(file_token->value.str_value, O_WRONLY | O_CREAT | O_APPEND,
+				0644));
+	else if (file_token->type == TOKEN_HEREDOC)
+		return (file_token->value.fd_value);
+	return (-1);
+}
+
+void	redirect(t_data *data, t_token *file_obj)
+{
+	int	fd;
+
+	if (file_obj->value.fd_value == AMBIGUOUS_REDIRECTION)
+	{
+		ft_putstr_fd("minishell: ambiguous redirect\n", 2);
+		exit_status(data, 1);
+	}
+	fd = open_file(file_obj);
+	if (fd != -1)
+	{
+		if (file_obj->type == TOKEN_APPEND
+			|| file_obj->type == TOKEN_REDIRECT_OUT)
+			dup2(fd, STDOUT_FILENO);
+		else
+			dup2(fd, STDIN_FILENO);
+	}
+	else
+	{
+		print_err(strerror(errno), file_obj->value.str_value);
+		exit_status(data, 1);
+	}
+	close(fd);
+}
+
+bool	is_ambiguous_redirect(t_token *token)
+{
+	if (token->value.fd_value == AMBIGUOUS_REDIRECTION)
+	{
+		ft_putstr_fd("minishell: ambiguous redirect\n", 2);
+		return (true);
+	}
+	return (false);
+}
+
+bool	open_output_redirect(t_data *data, t_token *token, int *last_idx,
+		int idx)
+{
+	data->out_fd = open_file(token);
+	if (data->out_fd < 0)
+	{
+		print_err(strerror(errno), token->value.str_value);
+		return (false);
+	}
+	if (!isatty(data->out_fd))
+		*last_idx = idx;
+	return (true);
+}
--- execution/redirections.c
+++ execution/redirections.c
@@ -1,32 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbousset <mbousset@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 17:05:18 by mbousset          #+#    #+#             */
+/*   Updated: 2025/05/05 17:05:38 by mbousset         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "execution.h"
 
+bool	open_input_redirect(t_data *data, t_token *token)
+{
+	data->out_fd = open_file(token);
+	if (data->out_fd < 0)
+	{
+		print_err(strerror(errno), token->value.str_value);
+		return (false);
+	}
+	close(data->out_fd);
+	return (true);
+}
 
+bool	handle_redirect_token(t_data *data, t_token *token, int *last_idx,
+		int idx)
+{
+	if (data->out_fd != 1)
+		close(data->out_fd);
+	if (is_ambiguous_redirect(token))
+	{
+		data->out_fd = -1;
+		return (false);
+	}
+	if (token->type == TOKEN_APPEND || token->type == TOKEN_REDIRECT_OUT)
+		return (open_output_redirect(data, token, last_idx, idx));
+	else
+		return (open_input_redirect(data, token));
+}
 
-void	perforem_subshell_redirs(t_data *data, int n)
+bool	process_redirections(t_data *data, t_token **redir_lst, int *last_idx)
 {
 	size_t	i;
-	t_token	**redir_lst;
 
 	i = 0;
-	if (data->lst_cmd[n].subshell_node->redirect_list)
-		redir_lst = (t_token **)data->lst_cmd[n].subshell_node->redirect_list->items;
-	else
-		redir_lst = NULL;
-	if (n != data->cmd_nbr - 1)
-		close(data->fd[0]);
-	if (data->old_fd != -1)
-	{
-		dup2(data->old_fd, STDIN_FILENO);
-		close(data->old_fd);
-	}
-	if (n != data->cmd_nbr - 1)
-	{
-		dup2(data->fd[1], STDOUT_FILENO);
-		close(data->fd[1]);
-	}
 	while (redir_lst && redir_lst[i])
 	{
-		redirect(data, redir_lst[i]);
+		if (!handle_redirect_token(data, redir_lst[i], last_idx, i))
+			return (false);
 		i++;
 	}
+	return (true);
+}
+
+bool	reopen_last_output(t_data *data, t_token **redir_lst, int last_idx)
+{
+	if (last_idx != -1)
+	{
+		close(data->out_fd);
+		data->out_fd = open_file(redir_lst[last_idx]);
+	}
+	return (true);
+}
+
+bool	redirection_builtins(t_data *data, int n)
+{
+	t_token	**redir_lst;
+	int		last_not_atty;
+
+	last_not_atty = -1;
+	redir_lst = data->lst_cmd[n].redirlist;
+	reset_fds(data, n);
+	if (!process_redirections(data, redir_lst, &last_not_atty))
+		return (false);
+	return (reopen_last_output(data, redir_lst, last_not_atty));
 }
