c84fdb2fbd41cba545b1429beaab709cef752382
Soulaimane Alahiane <salahian@e1r12p6.1337.ma>
2025-05-07 09:16:06 +0100

    still need fixing

--- field_splitting.c
+++ field_splitting.c
@@ -6,7 +6,7 @@
 /*   By: salahian <salahian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 15:21:24 by salahian          #+#    #+#             */
-/*   Updated: 2025/05/03 15:44:27 by salahian         ###   ########.fr       */
+/*   Updated: 2025/05/07 09:05:15 by salahian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,81 @@ int	check_first_char(char c)
 {
 	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_');
 }
+
+int count_nbr_words(char *str, char *field)
+{
+    int i;
+    int in_word;
+    int count;
+
+    i = 0;
+    in_word = 0;
+    count = 0;
+    while (str[i])
+    {
+        if ((str[i] != ' ' && str[i] != '\t') || field[i] != '1')
+        {
+            if (!in_word)
+            {
+                in_word = 1;
+                count++;
+            }
+        }
+        else
+            in_word = 0;
+        i++;
+    }
+    return count;
+}
+
+static char *allocat(const char *s, char *field, int *j)
+{
+    int start;
+    int len;
+    int i;
+    char *w;
+
+    while (s[*j] && (s[*j] == ' ' || s[*j] == '\t') && field[*j] == '1')
+        (*j)++;
+    start = *j;
+    while (s[*j] && ((s[*j] != ' ' && s[*j] != '\t') || field[*j] != '1'))
+        (*j)++;
+    len = *j - start;
+    w = malloc(len + 1);
+    if (!w)
+        return NULL;
+    i = 0;
+    while (i < len)
+    {
+        w[i] = s[start + i];
+        i++;
+    }
+    w[len] = '\0';
+    return (w);
+}
+
+char **applicate_splitting(char *str, char *field)
+{
+    int count;
+    char **new;
+    int j;
+    int i;
+
+    count = count_nbr_words(str, field);
+    new = ft_calloc(count + 1, sizeof(char *));
+    j = 0;
+    i = 0;
+    while (i < count)
+    {
+        new[i] = allocat(str, field, &j);
+        if (!new[i])
+            return NULL;
+        i++;
+    }
+    new[i] = NULL;
+    return new;
+}
+
 
 int	check_the_last_arg(char *tmp)
 {
