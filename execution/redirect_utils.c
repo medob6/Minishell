66aafe7fd01cff041b895ec9743c00e067ba0217
Mohamed Boussetta <mbousset@e1r12p2.1337.ma>
2025-05-17 14:55:28 +0100

    still need teting

--- execution/redirect_utils.c
+++ execution/redirect_utils.c
@@ -6,7 +6,7 @@
 /*   By: mbousset <mbousset@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:00:15 by mbousset          #+#    #+#             */
-/*   Updated: 2025/05/14 10:19:04 by mbousset         ###   ########.fr       */
+/*   Updated: 2025/05/17 14:28:21 by mbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ int	open_file(t_str *file_token)
 	if (file_token->type == TOKEN_REDIRECT_IN)
 		return (open(file_token->value[0], O_RDONLY));
 	else if (file_token->type == TOKEN_REDIRECT_OUT)
-		return (open(file_token->value[0], O_WRONLY | O_CREAT | O_TRUNC,
-				0644));
+		return (open(file_token->value[0], O_WRONLY | O_CREAT | O_TRUNC, 0644));
 	else if (file_token->type == TOKEN_APPEND)
 		return (open(file_token->value[0], O_WRONLY | O_CREAT | O_APPEND,
 				0644));
@@ -62,16 +61,17 @@ bool	is_ambiguous_redirect(t_str *token)
 	return (false);
 }
 
-bool	open_output_redirect(t_data *data, t_str *token, int *last_idx,
-		int idx)
+bool	open_output_redirect(t_data *data, t_str *token, int *last_idx, int idx)
 {
+
 	data->out_fd = open_file(token);
 	if (data->out_fd < 0)
 	{
 		print_err(strerror(errno), token->value[0]);
 		return (false);
 	}
-	if (!isatty(data->out_fd))
-		*last_idx = idx;
+
+	if (ft_strcmp(token->value[0], "/dev/stdout"))
+				*last_idx = idx;
 	return (true);
 }
